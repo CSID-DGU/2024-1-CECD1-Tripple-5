7d8c324856598b4618b96c150de0728f5db36e14
parkikbum <pibum@naver.com>
2024-06-06 23:56:33 +0900

    Add: add proj

---
+++ TalkTravel/TalkTravel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TalkTravel
+//
+//  Created by 박익범 on 6/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
