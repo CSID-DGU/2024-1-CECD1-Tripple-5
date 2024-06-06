18c86c75b547d52cf693b96e8fbf7d603aa0d9aa
l2zh <113578158+l2zh@users.noreply.github.com>
2024-06-06 20:51:47 +0900

    Revert "[UI] 기본페이지 세팅 및 채팅 뷰작업 완료"

--- app/src/main/java/com/example/traveltalk/domain/repository/DummyRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.traveltalk.domain.repository
-
-import com.example.traveltalk.data.model.request.RequestDummyDto
-import com.example.traveltalk.domain.entity.DummyInfoList
-
-interface DummyRepository {
-
-    suspend fun insertDummy(requestDummyDto: RequestDummyDto): Result<DummyInfoList>
-}
