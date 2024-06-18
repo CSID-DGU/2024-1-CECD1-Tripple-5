be024d2295afb7c5a27edc5d1b06e100b6e391f9
khoon9 <kshun08@dongguk.edu>
2024-06-18 19:18:53 +0900

    Chore: 개발 편의 설정
    - 매 실행마다 db drop 후 create 되도록 함
    - 추후 drop 주석처리 필요

--- server/app/database.py
+++ server/app/database.py
@@ -9,7 +9,12 @@ load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
 
 engine = create_async_engine(DATABASE_URL, echo=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
+SessionLocal = sessionmaker(
+    autocommit=False, 
+    autoflush=False, 
+    bind=engine, 
+    class_=AsyncSession
+)
 Base = declarative_base()
 
 async def get_db():
--- server/app/main.py
+++ server/app/main.py
@@ -2,24 +2,14 @@ from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from app import models, schemas, crud
 from app.database import engine, get_db
+from .routers import chat
 
 app = FastAPI()
 
 @app.on_event("startup")
 async def startup():
     async with engine.begin() as conn:
-        await conn.run_sync(models.Base.metadata.create_all)
+        await conn.run_sync(models.Base.metadata.drop_all)  # 모든 테이블 드롭 <- 배포단계에서 지울 것
+        await conn.run_sync(models.Base.metadata.create_all)  # 테이블 다시 생성
 
-@app.post("/users/", response_model=schemas.User)
-async def create_user(user: schemas.UserCreate, db: AsyncSession = Depends(get_db)):
-    db_user = await crud.get_user_by_username(db, username=user.username)
-    if db_user:
-        raise HTTPException(status_code=400, detail="Username already registered")
-    return await crud.create_user(db=db, user=user)
-
-@app.get("/users/{user_id}", response_model=schemas.User)
-async def read_user(user_id: int, db: AsyncSession = Depends(get_db)):
-    db_user = await crud.get_user(db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
+app.include_router(chat.router, prefix="/api/v1", tags=["chat"])
