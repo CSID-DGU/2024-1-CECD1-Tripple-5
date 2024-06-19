cdf474fd49379ff698af15be9743633dc37683e8
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2024-06-20 02:03:43 +0900

    :sparkles: Feat: langcahin 연동
    feat: langcahin 연동

--- server/app/service/chatbot/chat.py
+++ server/app/service/chatbot/chat.py
@@ -2,6 +2,12 @@ from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain.callbacks import StreamingStdOutCallbackHandler
 
+from .utils2 import init_llm, embed_file, create_retriever, save_message, send_message, generate_chain, handle_message, retriever_map, llm
+from .prompts import prompt_keyword
+
+from .prompts import prompt_keyword, prompt_place
+from fastapi.responses import JSONResponse
+
 chat = ChatOpenAI(
     temperature=0.1,
     streaming=True,
@@ -20,4 +26,17 @@ chat_prompt = ChatPromptTemplate.from_messages(
 chat_chain = chat_prompt | chat
 
 async def get_response_from_chatgpt(message):
-    return chat_chain.invoke({"question": message}).content
+    # message
+    chain = generate_chain(retriever_map["keyword"], prompt_keyword)
+    keyword = chain.invoke(message).content
+    if "대분류 : " in keyword:
+        keyword = keyword.replace("대분류 : ", "").strip()
+    print("추출된 키워드 ------------------ : ", keyword)
+    # keyword 추출하기 
+    if keyword in retriever_map:
+        return handle_message(message, keyword)
+    else:
+        return JSONResponse(content={"error": "잘못된 입력입니다. 다시 시도해주세요."})
+
+    # message
+    # return chat_chain.invoke({"question": message}).content
